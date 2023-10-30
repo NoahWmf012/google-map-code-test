68ee6bf22ba0fc602090745b18d78959246f8a83
NoahWmf012 <wumanfung1@gmail.com>
2023-10-30 15:59:27 -0400

    add marker on the map

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -6,17 +6,20 @@
             <input type="text" placeholder="Enter your address" @keyup.enter="geocode" v-model="address"
                 ref="autocomplete" />
             <button class="btn btn-primary" @click="geocode">GO</button>
-            <button class="btn btn-info" :disabled="records.length == 0" @click="this.showTable = !this.showTable">
-                Show Records</button>
+            <button class="btn btn-info" :disabled="records.length === 0" @click="showTable = !showTable">
+                {{ showTable ? "Hide Records" : "Show Records" }}
+            </button>
         </div>
 
         <!-- data table -->
         <div id="data-table" style="position:relative;z-index:1" v-if="showTable">
+            <p>{{ markers }}</p>
             <table class="table">
                 <tbody>
-                    <tr v-for="(record, index) in records" :key="record">
-                        <th scope="row">{{ index + 1 }}</th>
-                        <td>{{ record }}</td>
+                    <tr v-for="(record, index) in filteredRecords" :key="index">
+                        <input v-model="markers" type="checkbox" :id="record.name" :value="record" />
+                        <th scope="row">{{ (currentPage - 1) * rowsPerPage + index + 1 }}</th>
+                        <td>{{ record.name }}</td>
                     </tr>
                 </tbody>
             </table>
@@ -39,8 +42,10 @@
         <section id="map" ref="map"></section>
     </div>
 </template>
+  
 <script>
 import { geocodeAddress } from '../services/geocodingService';
+import { toRaw } from 'vue';
 
 export default {
     name: "MapComponent",
@@ -50,55 +55,55 @@ export default {
             geocodingResult: null,
             showMap: false,
             records: [],
+            markers: [],
             showTable: false,
             currentPage: 1,
             totalPages: 1,
-            rowsPerPage: 10
+            rowsPerPage: 10,
+            map: { location: "" }
         };
     },
     methods: {
         async geocode() {
             try {
                 this.geocodingResult = await geocodeAddress(this.address);
-                const { lat, lng } = this.geocodingResult.results[0].geometry.location
-                this.fetchLocation(lat, lng, this.geocodingResult.results[0].formatted_address)
+                const { lat, lng } = this.geocodingResult.results[0].geometry.location;
+                this.fetchLocation(lat, lng, this.geocodingResult.results[0].formatted_address);
             } catch (error) {
-                alert(error)
+                alert(error);
             }
         },
         fetchCurrentLocation() {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                    (position) => {
-                        this.fetchLocation(
-                            position.coords.latitude,
-                            position.coords.longitude
-                        );
-                    }
-                )
-                this.showMap = true
+                navigator.geolocation.getCurrentPosition((position) => {
+                    this.fetchLocation(position.coords.latitude, position.coords.longitude);
+                });
+                this.showMap = true;
             } else {
-                console.log("Your browser does not allow to  geolocation API ");
+                console.log("Your browser does not allow geolocation API.");
             }
         },
         fetchLocation(latitude, longitude, name = "") {
-            // Show & center the Map based oon
+            // Show & center the Map based on latitude and longitude
             var map = new window.google.maps.Map(this.$refs["map"], {
                 zoom: 15,
                 center: new window.google.maps.LatLng(latitude, longitude),
                 mapTypeId: window.google.maps.MapTypeId.ROADMAP,
             });
-
             new window.google.maps.Marker({
                 position: new window.google.maps.LatLng(latitude, longitude),
                 map: map,
             });
-            if (name != undefined && name.length > 0) {
-                this.records.unshift(name)
+
+            if (name !== undefined && name.length > 0) {
+                this.records.unshift({ name, latitude, longitude });
             }
+            // Update the total pages based on the number of records
+            this.totalPages = Math.ceil(this.records.length / this.rowsPerPage);
+            this.map = map
         },
 
-        //pagination
+        // Pagination methods
         previousPage() {
             this.currentPage--;
         },
@@ -109,16 +114,29 @@ export default {
             this.currentPage = pageNum;
         }
     },
-
+    watch: {
+        markers(newValue, oldValue) {
+            console.log(">>>", newValue, oldValue)
+            const _map = toRaw(this.map)
+            const _newValue = toRaw(newValue)
+            for (var location in _newValue) {
+                new window.google.maps.Marker({
+                    position: new window.google.maps.LatLng(_newValue[location].latitude, _newValue[location].longitude),
+                    map: _map,
+                });
+            }
+        }
+    },
     computed: {
         filteredRecords() {
-            const startIndex = (this.currentPage - 1) * this.recordsPerPage;
-            const endIndex = startIndex + this.recordsPerPage;
+            const startIndex = (this.currentPage - 1) * this.rowsPerPage;
+            const endIndex = startIndex + this.rowsPerPage;
             return this.records.slice(startIndex, endIndex);
         }
     },
-}
+};
 </script>
+  
 <style>
 #map {
     position: absolute;
@@ -126,6 +144,6 @@ export default {
     right: 0;
     top: 0;
     bottom: 0;
-    background-color: brown;
+    background-color: grey;
 }
 </style>
